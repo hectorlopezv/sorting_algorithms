f64d0406bdd7d7cceb0cb75e68f57700b144a918
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-06-18 01:34:28 -0500

    fix cocktail

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 listint_t *bubble_rigth(listint_t **list, listint_t **start,
-		listint_t **end, int *sorted, listint_t **start_mov);
+		listint_t **end, int *sorted);
 
 void bubble_left(listint_t **list, listint_t **end, listint_t **start_mov,
 		int *sorted);
@@ -30,7 +30,7 @@ void cocktail_sort_list(listint_t **list)
 	start_mov = NULL;
 	do {
 		sorted = 0;
-		end = bubble_rigth(list, &start, &end, &sorted, &start_mov);
+		end = bubble_rigth(list, &start, &end, &sorted);
 		if (sorted == 0)
 			break;
 		sorted = 0;
@@ -38,7 +38,12 @@ void cocktail_sort_list(listint_t **list)
 		end_mov = end;
 		bubble_left(list, &end, &start_mov, &sorted);
 
-		start = start_mov->next;
+		if (start_mov->prev != NULL)
+			start = start_mov->next;
+		else
+		{
+			start = start_mov;
+		}
 		if (end->next != NULL)
 			end = end_mov->prev;
 
@@ -50,21 +55,21 @@ void cocktail_sort_list(listint_t **list)
  * bubble_rigth - bubble_rigth
  * @start: start limit of bubble sort(left limit)
  * @list: list of nodes
- * @start_mov: the left limit of bubble sort(left limit)
  * @sorted: sort boolean
  * @end: rigth limit of bubble sort
  * Return: return starting point
  */
 listint_t *bubble_rigth(listint_t **list, listint_t **start,
-		listint_t **end, int *sorted, listint_t **start_mov)
+		listint_t **end, int *sorted)
 {
-	listint_t *prev, *next, *end_t, *start_t;
+	(void) end;
+	listint_t *prev, *next, *start_t;
 
 	start_t = *start;
-	end_t = *end;
+	/*end_t = *end;*/
 	next = prev = NULL;
 
-	while (start_t->next != end_t)
+	while (start_t->next != NULL)
 	{
 		next = start_t->next;
 		if (start_t->n > next->n)
@@ -84,7 +89,7 @@ listint_t *bubble_rigth(listint_t **list, listint_t **start,
 			if (next->prev == NULL)
 			{
 				*list = next;
-				*start_mov = next;
+
 			}
 			start_t = next;
 			*sorted = 1;
@@ -92,6 +97,7 @@ listint_t *bubble_rigth(listint_t **list, listint_t **start,
 		}
 		prev  = start_t;
 		start_t = start_t->next;
+
 	}
 
 	return (start_t);
@@ -112,12 +118,13 @@ void bubble_left(listint_t **list, listint_t **end,
 
 	end_t = *end;
 
-	while (end_t->prev != *start_mov)
+	while (end_t->prev != NULL)
 	{
 		prev = end_t->prev;
 
 		if (end_t->n < prev->n)
 		{
+
 			end_t->prev = prev->prev;
 
 			if (prev->prev != NULL)
@@ -139,11 +146,10 @@ void bubble_left(listint_t **list, listint_t **end,
 			end_t = prev;
 			*sorted = 1;
 			print_list(*list);
+
 		}
 		end_t = end_t->prev;
 	}
-
 	if (*start_mov == NULL)
-		*start_mov = *list;
-
+		*start_mov = end_t;
 }
